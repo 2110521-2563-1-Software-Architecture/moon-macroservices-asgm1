215e85195ceef0ac774ab9b1dadd5c52a52238d7
icekang <namkang_chutisilp@hotmail.com>
2020-09-05 04:58:46 +0700

    try spawn

--- interface.js
+++ interface.js
@@ -1,18 +1,17 @@
+const { spawn } = require("child_process");
+
 var processName = process.argv.shift();
 var scriptName = process.argv.shift();
 var interface = process.argv.shift();
 var command = process.argv.shift();
 
-var grpcClient = require("./grpc/client.js");
 var restapiClient = require("./restAPI/client.js");
 console.log(interface);
 switch (interface) {
   case "grpc": {
-    if (command == "list") grpcClient.listBooks();
-    else if (command == "insert")
-      grpcClient.insertBook(process.argv[0], process.argv[1], process.argv[2]);
-    else if (command == "get") grpcClient.getBook(process.argv[0]);
-    else if (command == "delete") grpcClient.deleteBook(process.argv[0]);
+    ls = spawn("node", ["client.js", command].concat(process.argv), {
+      cwd: "./grpc",
+    });
   }
   case "restapi": {
     console.log("rest");
